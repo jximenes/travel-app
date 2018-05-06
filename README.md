d3533c6df86896d3889db4fb15965df34f864035
João Ximenes <jamximenes@gmail.com>
2018-05-06 22:33:05 +0100

    readme file update default layout for different viewport sizes

--- README.md
+++ README.md
@@ -15,13 +15,13 @@
 
 ## some references
 ### responsive web design
-https://developers.google.com/web/fundamentals/design-and-ux/responsive
-I used "mostly fluid" pattern for grid view with fixed width for larger viewports, and flex layout mode to arrange items on containers in most cases.
+- https://developers.google.com/web/fundamentals/design-and-ux/responsive
+- I used "mostly fluid" pattern for grid view with fixed width for larger viewports, and flex layout mode to arrange items on containers in most cases.
 
 ### responsive web design using flex. mobile-first
-https://internetingishard.com/html-and-css/responsive-design
-https://internetingishard.com/html-and-css/flexbox
+- https://internetingishard.com/html-and-css/responsive-design
+- https://internetingishard.com/html-and-css/flexbox
 
 ### sass media queries breakpoints
-https://medium.freecodecamp.org/the-100-correct-way-to-do-css-breakpoints-88d6a5ba1862
-https://www.sitepoint.com/managing-responsive-breakpoints-sass
+- https://medium.freecodecamp.org/the-100-correct-way-to-do-css-breakpoints-88d6a5ba1862
+- https://www.sitepoint.com/managing-responsive-breakpoints-sass
--- app/products-block/products-block.directive.js
+++ app/products-block/products-block.directive.js
@@ -3,20 +3,36 @@
 
     angular
         .module("travelApp")
-        .directive("productsBlock", [function(){
+        .directive("productsBlock", ["$window", function($window){
+            var smallViewportSizeUp = 900; // for viewports bigger than 900px layout is list
             /**
              * productsBlock directive allows to show a list of products based on products-block.template.html
              * this directive receives as parameter:
              * info - object with the product name and the products array
+             * layout - layout type (grid/list/default). if default then for small viewports layout is grid, and for big viewports layout is list
              */
             var directive = {
                 restrict: "A",
                 scope:{
-                    info: "="
+                    info: "=",
+                    defaultLayout: "="
                 },
                 templateUrl : "app/products-block/products-block.template.html",
                 controller: ["$scope", function($scope){
-                    $scope.layout = $scope.layout || "grid";
+                    
+                    //calculate viewport width to apply correct layout
+                    if($scope.defaultLayout == "default"){
+                        var $w = angular.element($window);
+                        if($w[0].innerWidth < smallViewportSizeUp){
+                            $scope.layout = "grid";
+                        }
+                        else{
+                            $scope.layout = "list";
+                        }
+                    }
+                    else{
+                        $scope.layout = $scope.defaultLayout || "grid";
+                    }
 
                     /**
                      * this function is used to switch between grid and list layouts
